882f9f3a9c4bec5fbf8e9b560aad7a48071da33c
Justin Chang <justinxchang@hotmail.com>
2018-08-17 02:55:40 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -10,5 +10,10 @@ function destructivelyAddElementToBeginningOfArray(array, element){
 }
 
 function addElementToEndOfArray(array, element){
-  
+    return [...array, element]
+}
+
+function destructivelyAddElementToEndOfArray(array, element){
+  array.push(element);
+  return array
 }
